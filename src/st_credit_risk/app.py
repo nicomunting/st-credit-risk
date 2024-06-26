3989ecabf68c10c99ae66132c1494936daa3a4b2
Nico Munting <nico.munting@muntech.nl>
2024-06-26 16:53:28 +0200

    Improve layout and add title

--- src/st_credit_risk/app.py
+++ src/st_credit_risk/app.py
@@ -7,6 +7,11 @@ import streamlit as st
 from utils import vars, var_labels
 
 
+st.set_page_config("Credit Risk EDA", page_icon=":credit_card:", layout="wide")
+
+st.title("Credit Risk - Exploratory Data Analysis")
+
+
 # From https://www.kaggle.com/datasets/laotse/credit-risk-dataset/
 df = pd.read_csv(os.path.join("..", "..", "data", "credit_risk_dataset.csv"))
 
@@ -15,22 +20,30 @@ df["loan_status"] = (
     .cat.rename_categories({0: "Non-Default", 1: "Default"})
 )
 
-cat_order = {"loan_status": ["Non-Default", "Default"]}
+categories_order = {
+    "loan_status": ["Non-Default", "Default"],
+    "loan_grade": ["A", "B", "C", "D", "E", "F", "G"],
+}
 
 tabs = st.tabs(["Overview", "Univariate"])
 
 with tabs[0]:
-    count_fig = px.histogram(df, x="loan_status", category_orders=cat_order, labels=var_labels)
-    st.plotly_chart(count_fig)
+    col1, col2 = st.columns([1, 3], gap="large")
+    with col1:
+        count_fig = px.histogram(df, x="loan_status", category_orders=categories_order, labels=var_labels)
+        st.plotly_chart(count_fig, use_container_width=False)
 
-    st.write(df)
+    with col2:
+        st.write(df)
 
 with tabs[1]:
-    variable = st.selectbox("Variable", vars, format_func=lambda x: var_labels[x])
+    col1, col2 = st.columns([1, 3], gap="large")
+
+    variable = col1.selectbox("Variable", vars, format_func=lambda x: var_labels[x])
     var_is_numeric = variable in df.select_dtypes(include="number")
 
     if var_is_numeric:
-        value_range = st.slider(
+        value_range = col2.slider(
             "Value range", 
             min_value=df[variable].min(), max_value=df[variable].max(), 
             value=(df[variable].min(), df[variable].max())
@@ -42,7 +55,8 @@ with tabs[1]:
     hist_fig = px.histogram(
         hist_df, 
         x=variable, color="loan_status", 
-        category_orders=cat_order, labels=var_labels,
+        category_orders=categories_order, labels=var_labels,
+        marginal="rug" if var_is_numeric else None
     )
     st.plotly_chart(hist_fig, use_container_width=True)
 
@@ -50,7 +64,7 @@ with tabs[1]:
         violin_fig = px.violin(
             df, 
             x=variable, color="loan_status", 
-            category_orders=cat_order, labels=var_labels,
+            category_orders=categories_order, labels=var_labels,
             points="outliers", box=True
         )
         st.plotly_chart(violin_fig, use_container_width=True)
