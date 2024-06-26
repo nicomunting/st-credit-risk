fa95a2b0e7c3da587252511d2d2028c415a1ad36
Nico Munting <nico.munting@muntech.nl>
2024-06-26 14:40:44 +0200

    Improve histogram plot; add slider

--- src/st_credit_risk/app.py
+++ src/st_credit_risk/app.py
@@ -4,7 +4,10 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+from utils import vars, var_labels
 
+
+# From https://www.kaggle.com/datasets/laotse/credit-risk-dataset/
 df = pd.read_csv(os.path.join("..", "..", "data", "credit_risk_dataset.csv"))
 
 df["loan_status"] = (
@@ -14,11 +17,26 @@ df["loan_status"] = (
 
 cat_order = {"loan_status": ["Non-Default", "Default"]}
 
-count_fig = px.histogram(df, x="loan_status", category_orders={"loan_status": ["Non-Default", "Default"]})
+count_fig = px.histogram(df, x="loan_status", category_orders=cat_order, labels=var_labels)
 st.plotly_chart(count_fig)
 
-variable = st.selectbox("Variable", ["person_age", "person_income", "person_home_ownership"])
-hist_fig = px.histogram(df, x=variable, color="loan_status", category_orders=cat_order)
+variable = st.selectbox("Variable", vars, format_func=lambda x: var_labels[x])
+
+if variable in df.select_dtypes(include="number"):
+    value_range = st.slider(
+        "Value range", 
+        min_value=df[variable].min(), max_value=df[variable].max(), 
+        value=(df[variable].min(), df[variable].max())
+    )
+    hist_df = df.loc[df[variable].between(value_range[0], value_range[1]), :]
+else:
+    hist_df = df
+
+hist_fig = px.histogram(
+    hist_df, 
+    x=variable, color="loan_status", 
+    category_orders=cat_order, labels=var_labels,
+)
 st.plotly_chart(hist_fig)
 
 st.write(df)
---
+++ src/st_credit_risk/utils.py
@@ -0,0 +1,14 @@
+var_labels = {
+    "person_age": "Age", 
+    "person_income": "Income", 
+    "person_home_ownership": "Home Ownership",
+    "person_emp_length": "Length of Employment",
+    "loan_intent": "Loan Intent",
+    "loan_grade": "Loan Grade",
+    "loan_amnt": "Loan Amount",
+    "loan_int_rate": "Loan Interest Rate",
+    "loan_percent_income": "Loan to Income",
+    "loan_status": "Loan Status",
+}
+
+vars = list(var_labels.keys())
